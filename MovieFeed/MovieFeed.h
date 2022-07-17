d08a3d64e7242e1f5e38f856fe89756c27da6b66
shotikoKlibadze <sh.klibadze@gmail.com>
2022-07-17 12:03:43 +0400

    Initial Commit

---
+++ MovieFeed/MovieFeed.h
@@ -0,0 +1,18 @@
+//
+//  MovieFeed.h
+//  MovieFeed
+//
+//  Created by Shotiko Klibadze on 17.07.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MovieFeed.
+FOUNDATION_EXPORT double MovieFeedVersionNumber;
+
+//! Project version string for MovieFeed.
+FOUNDATION_EXPORT const unsigned char MovieFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MovieFeed/PublicHeader.h>
+
+

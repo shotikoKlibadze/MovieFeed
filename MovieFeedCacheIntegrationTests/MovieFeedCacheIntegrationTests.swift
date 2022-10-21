0482ef5566b0e8f4b0f6dc15980b61d19d162cba
Shota Klibadze <sh.klibadze@gmail.com>
2022-10-21 15:27:38 +0400

    added cache integration test scheme

---
+++ MovieFeedCacheIntegrationTests/MovieFeedCacheIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  MovieFeedCacheIntegrationTests.swift
+//  MovieFeedCacheIntegrationTests
+//
+//  Created by Shotiko Klibadze on 21.10.22.
+//
+
+import XCTest
+
+final class MovieFeedCacheIntegrationTests: XCTestCase {
+
+   
+
+}

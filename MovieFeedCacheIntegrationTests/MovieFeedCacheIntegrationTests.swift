586338c5eedfc4f4c31457f1a36c2324d8b5b0dd
Shota Klibadze <sh.klibadze@gmail.com>
2022-10-21 16:46:49 +0400

    added test to CI Scheme

--- MovieFeedCacheIntegrationTests/MovieFeedCacheIntegrationTests.swift
+++ MovieFeedCacheIntegrationTests/MovieFeedCacheIntegrationTests.swift
@@ -6,9 +6,91 @@
 //
 
 import XCTest
+import MovieFeed
+import MovieFeedTests
 
 final class MovieFeedCacheIntegrationTests: XCTestCase {
+    
+    override func tearDown() {
+        super.tearDown()
+        deletStoreArtifacts()
+    }
+    
+    override func setUp() {
+        super.setUp()
+        deletStoreArtifacts()
+    }
+    
+    func test_loadFeed_deliversNoItemsOnEmptyCache() {
+        let sut = makeFeedLoader()
+        
+        let exp = expectation(description: "wait for load completion")
+        
+        sut.load { result in
+            switch result {
+            case .success(let items):
+                XCTAssertEqual(items, [])
+            case .failure(_):
+                XCTFail()
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_load_deliversitemsSavedOnAseperateInsance() {
+        let savingSUT = makeFeedLoader()
+        let retrieveingSUT = makeFeedLoader()
+        
+        let items = uniqueItems().models
+        let savExp = expectation(description: "wait for saving")
+        savingSUT.save(items: items) { error in
+            XCTAssertNil(error)
+            savExp.fulfill()
+        }
+        wait(for: [savExp], timeout: 1.0)
+        
+        let retrieveExp = expectation(description: "wait for retrieving")
+        retrieveingSUT.load { result in
+            switch result {
+                
+            case .success(let recievedItems):
+                XCTAssertEqual(recievedItems, items)
+            case .failure(_):
+                XCTFail("Not THe Same")
+            }
+            retrieveExp.fulfill()
+        }
+        wait(for: [retrieveExp], timeout: 1.0)
+    }
+    
+    func test_save_overridesItemsSavedOnASeperateInstance() {
+        
+    }
+    
 
-   
+    // MARK: - Helpers
+    
+    private func makeFeedLoader(currentDate: Date = Date(), file: StaticString = #filePath, line: UInt = #line) -> LocalFeedLoader {
+        let storeURL = testSpecificStoreURL()
+        print(storeURL)
+        let store = try! CoreDataFeedStore(storeURL: storeURL)
+        let sut = LocalFeedLoader(store: store, currentDate: { currentDate })
+        trackForMemoryLeacks(isntance: sut)
+        return sut
+    }
+        
+    private func testSpecificStoreURL() -> URL {
+        return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    private func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+    
+    private func deletStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+    }
 
 }
